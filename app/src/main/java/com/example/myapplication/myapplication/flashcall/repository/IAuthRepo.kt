aebef9e042a208be00149883a88692d76d5f12d7
Sujit Kumar Das <sanjib121kumardas@gmail.com>
2024-07-09 20:30:25 +0530

    Validation API Improvements

--- app/src/main/java/com/example/myapplication/myapplication/flashcall/repository/IAuthRepo.kt
+++ app/src/main/java/com/example/myapplication/myapplication/flashcall/repository/IAuthRepo.kt
@@ -5,6 +5,7 @@ import com.example.myapplication.myapplication.flashcall.Data.model.CreateUserRe
 import com.example.myapplication.myapplication.flashcall.Data.model.ResendOTPResponse
 import com.example.myapplication.myapplication.flashcall.Data.model.SendOTPResponseX
 import com.example.myapplication.myapplication.flashcall.Data.model.VerifyOTPResponse
+import com.example.myapplication.myapplication.flashcall.Data.model.chatDataModel.ValidateResponse
 import kotlinx.coroutines.flow.Flow
 
 interface IAuthRepo {
@@ -14,6 +15,8 @@ interface IAuthRepo {
     suspend fun resendOtp(url: String, number: String): Flow<ResendOTPResponse>
 
     suspend fun verifyOtp(url: String, number: String, otp: String, token: String): Flow<VerifyOTPResponse>
+
+    suspend fun validateUser(url : String, token : String) : Flow<ValidateResponse>
 
 
 }
