d82b95f0ce8b52a7e360d422e724901494fc6096
utkarsh singh <78771861+utkarshxf@users.noreply.github.com>
2024-10-07 21:07:05 +0530

    feat: push notification

--- app/src/main/java/com/example/myapplication/myapplication/flashcall/Screens/callServices/AcceptCallActivity.kt
+++ app/src/main/java/com/example/myapplication/myapplication/flashcall/Screens/callServices/AcceptCallActivity.kt
@@ -1,11 +1,19 @@
 package com.example.myapplication.myapplication.flashcall.Screens.callServices
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.lifecycle.lifecycleScope
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import dagger.hilt.android.AndroidEntryPoint
 import io.getstream.video.android.core.StreamVideo
 import io.getstream.video.android.core.notifications.NotificationHandler
@@ -24,23 +32,56 @@ class AcceptCallActivity : ComponentActivity() {
                 finishAndRemoveTask()
                 return@launch
             }
+            var isPermissionGiven = true
             val call = StreamVideo.instance().call(callId.type, callId.id)
+            Log.e("AcceptCallActivity123" , call.state.createdBy.value?.id.toString())
+            Log.e("AcceptCallActivity123" , call.state.participants.value.toString())
+            Log.e("AcceptCallActivity123" , call.state.endedByUser.value.toString())
+            Log.e("AcceptCallActivity123" , call.state.connection.value.toString())
+            Log.e("AcceptCallActivity123" , call.state.endedAt.value.toString())
+            Log.e("AcceptCallActivity123" , call.state.toString())
             setContent {
                 CompositionLocalProvider(
                     androidx.lifecycle.compose.LocalLifecycleOwner provides androidx.compose.ui.platform.LocalLifecycleOwner.current,
                 ) {
-                    lifecycleScope.launch {
-                        call.join()
+                    EnsureVideoCallPermissions{
+                        isPermissionGiven = true
                     }
-                    CallScreen(call = call, showDebugOptions = true, onCallDisconnected = {
-                        call.leave()
-                        finishAndRemoveTask()
-                    }, onUserLeaveCall = {
-                        call.leave()
-                        finishAndRemoveTask()
-                    })
+                    if(isPermissionGiven)
+                    {
+                        lifecycleScope.launch {
+                            call.join()
+                        }
+                        CallScreen(call = call, showDebugOptions = true, onCallDisconnected = {
+                            call.leave()
+                            finishAndRemoveTask()
+                        }, onUserLeaveCall = {
+                            call.leave()
+                            finishAndRemoveTask()
+                        })
+                    }
                 }
             }
+        }
+    }
+}
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+private fun EnsureVideoCallPermissions(onPermissionsGranted: () -> Unit) {
+    val permissionsState = rememberMultiplePermissionsState(permissions = buildList {
+        // Access to microphone
+        add(Manifest.permission.CAMERA)
+        add(Manifest.permission.RECORD_AUDIO)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            add(Manifest.permission.FOREGROUND_SERVICE)
+        }
+    })
+    LaunchedEffect(key1 = Unit) {
+        permissionsState.launchMultiplePermissionRequest()
+    }
+    LaunchedEffect(key1 = permissionsState.allPermissionsGranted) {
+        if (permissionsState.allPermissionsGranted) {
+            onPermissionsGranted()
         }
     }
 }
